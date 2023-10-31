09dde3acbab1c2fa702c7a4f6e9501ec4886e73d
thepihen <reelpihen@gmail.com>
2023-10-31 17:05:17 +0200

    Extra comment

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { reactive } from 'vue'
-
+//this will be shared among all components,
+//and will be reactive. The logic is explained here:
+//https://vuejs.org/guide/scaling-up/state-management.html#simple-state-management-with-reactivity-api
 export const store = reactive({
     loggedIn: false,
     login(){
