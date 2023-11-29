79832987a276243fea9935dca140be205e4962e4
Jakub Votrubec <jakub.votrubec@recombee.com>
2023-11-29 15:19:03 +0200

    renamed column in db

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -10,12 +10,12 @@ class User(db.Model, UserMixin):
     id: int
     username: str
     password: str
-    seenTutorial: int
+    seen_tutorial: int
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    seenTutorial = db.Column(db.Integer, default=0)
+    seen_tutorial = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return f'<User "{self.username}">'
