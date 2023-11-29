b0784350ea8253fba519b28521576a906d227be4
Jakub Votrubec <jakub.votrubec@recombee.com>
2023-11-29 13:51:18 +0200

    config fixed typo

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_LEY = os.environ.get("SECRET_KEY") or "FJR-secret1324"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "FJR-secret1324"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URI"
     ) or "sqlite:///" + os.path.join(basedir, "syntha_db.db")
